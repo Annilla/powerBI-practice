b4382186deaa842dafa59b76dac79f05ed05e208
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2018-10-09 15:51:57 +0800

    base

---
+++ circleCard/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var circleCardFFD793C3973F441196C8C9F0273138C5_DEBUG = {
+        name: 'circleCardFFD793C3973F441196C8C9F0273138C5_DEBUG',
+        displayName: 'CircleCard',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '2.1.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.circleCardFFD793C3973F441196C8C9F0273138C5.Visual(options),
+        custom: true
+    };
+}
