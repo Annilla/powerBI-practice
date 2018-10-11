877f800eb0b28267f9af8d915c2da051a6d3b02f
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2018-10-11 17:42:50 +0800

    BarChart - Install Package

---
+++ barChart/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var barChartBC80E870F53F457F81A8959510AC6A85_DEBUG = {
+        name: 'barChartBC80E870F53F457F81A8959510AC6A85_DEBUG',
+        displayName: 'BarChart',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '2.1.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.barChartBC80E870F53F457F81A8959510AC6A85.Visual(options),
+        custom: true
+    };
+}
